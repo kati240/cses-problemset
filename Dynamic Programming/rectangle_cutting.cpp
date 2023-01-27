80f7543ea52c4a7213052be1c30a489d9db03e80
Kaustubh Tripathi <kaustubhtripathi@Kaustubhs-MacBook-Air.local>
2023-01-27 22:09:41 +0530

    rectangle cutting

---
+++ Dynamic Programming/rectangle_cutting.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int dp[a+1][b+1];
+    for(int i=0;i<=a;i++){
+        for(int j=0;j<=b;j++){
+            if(i==j){
+                dp[i][j]=0;
+            }
+            else{
+                dp[i][j]=1000000000;
+                for(int k=1;k<j;k++){
+                    dp[i][j]=min(dp[i][j],dp[i][k]+dp[i][j-k]+1);
+                }
+                for(int k=1;k<i;k++){
+                    dp[i][j]=min(dp[i][j],dp[k][j]+dp[i-k][j]+1);
+                }
+
+            }
+        }
+    }
+
+    cout<<dp[a][b];
+    return 0;
+}
